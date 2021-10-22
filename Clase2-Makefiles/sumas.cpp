195884881e263fb5709782caadeaa597d4747ea5
Nicolas Rueda <nicolasthrash25@hotmail.com>
2021-10-22 13:01:27 -0500

    Se generaliza makefile para crear graficos via gnuplot

--- Clase2-Makefiles/sumas.cpp
+++ Clase2-Makefiles/sumas.cpp
@@ -1,5 +1,6 @@
 //Comparar suma harmonica recorrida de dos formas distintas para ver las diferencias computacionales
 #include <iostream>
+#include <cmath>
 
 typedef float real;
 real sumup(int N);
@@ -10,8 +11,12 @@ int main(int argc, char const *argv[])
 	std::cout.precision(6);
 	std::cout.setf(std::ios::scientific);
 	int N=1000;
-	real dif= std::abs(sumup(N)-sumdown(N))/sumdown(N);
-	std::cout <<sumup(N)<<"\t" <<sumdown(N)<< "\t"<<dif <<"\n";
+	for (int i = 1; i < N; ++i)
+	{
+		real suma1 = sumup(i);
+		real suma2 = sumdown(i);
+		std::cout << i << "\t"<< std:: fabs(1-suma1/suma2)<<"\n";
+	}
 	return 0;
 }
 
