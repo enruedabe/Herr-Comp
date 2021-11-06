d79ede522b2d2c16e10aa4f2bd7439b628313dea
Nicolas Rueda <nicolasthrash25@hotmail.com>
2021-11-05 22:33:51 -0500

    Se anhade clase de debugging, falta .txt con instrucciones de ddd,ejemplos de valgrind y sanitizers

---
+++ Clase4-Debugging/Helloworld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+
+int main(int argc, char const *argv[])
+{
+	double a;
+	std::cout<< a << std::endl;
+	
+	float b=9.87;
+
+	float c = std::sin(b);
+	std::cout << c << std::endl;
+	return 0;
+}
